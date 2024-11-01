0b752686924871d11a354fd8e05ca9e18fa49af4
KartikAkolia <94763117+KartikAkolia@users.noreply.github.com>
2024-11-01 11:19:19 +0000

    Updated show_keybinds.sh
    I have added the ability to also execute the keybindings when hitting enter.

--- show_keybinds.sh
+++ show_keybinds.sh
@@ -1,8 +1,25 @@
 #!/bin/bash
 
+# Path to the Hyprland configuration file
 config_file=~/.config/hypr/hyprland.conf
+
+# Extract keybinds from the configuration file
 keybinds=$(grep -oP '(?<=bind = ).*' "$config_file")
 keybinds=$(echo "$keybinds" | sed 's/,\([^,]*\)$/ = \1/' | sed 's/, exec//g' | sed 's/^,//g')
 
-rofi -dmenu -p "Keybinds" -theme ~/.config/rofi/themes/nord.rasi <<< "$keybinds"
+# Show keybinds in Rofi and capture the selected command
+selected_bind=$(echo "$keybinds" | rofi -dmenu -p "Keybinds" -theme ~/.config/rofi/themes/nord.rasi)
+
+# Execute the selected command if not empty
+if [ -n "$selected_bind" ]; then
+    # Extract the command after '='
+    command_to_run=$(echo "$selected_bind" | sed -n 's/.*= \(.*\)/\1/p')
+    
+    # Check if the command is not empty and execute it
+    if [ -n "$command_to_run" ]; then
+        eval "$command_to_run"
+    else
+        echo "No command found to execute."
+    fi
+fi
 
