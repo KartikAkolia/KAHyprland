94dc281a993b7fe14000de70b2b2e2c029d26e75
KartikAkolia <94763117+KartikAkolia@users.noreply.github.com>
2024-11-01 12:40:55 +0000

    Update hyprinstall.sh
    Refactor

--- hyprinstall.sh
+++ hyprinstall.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Define variables for colored output
+# Define variables
 GREEN="$(tput setaf 2)[OK]$(tput sgr0)"
 RED="$(tput setaf 1)[ERROR]$(tput sgr0)"
 YELLOW="$(tput setaf 3)[NOTE]$(tput sgr0)"
@@ -10,118 +10,70 @@ LOG="install.log"
 # Set the script to exit on error
 set -e
 
+printf "$(tput setaf 2) Welcome to the Arch Linux Hyprland installer!\n $(tput sgr0)"
+sleep 2
+
+printf "$YELLOW PLEASE BACKUP YOUR FILES BEFORE PROCEEDING!\nThis script will overwrite some of your configs and files!"
+sleep 2
+
+printf "\n$YELLOW Some commands require you to enter your password in order to execute.\nIf you are worried about entering your password, you can cancel the script now with CTRL Q or CTRL C and review the contents of this script.\n"
+sleep 3
+
 # Function to print error messages
 print_error() {
-    printf "%s %s\n" "$RED" "$1" >&2
+    printf " %s%s\n" "$RED" "$1" >&2
 }
 
 # Function to print success messages
 print_success() {
-    printf "%s %s\n" "$GREEN" "$1"
+    printf "%s%s%s\n" "$GREEN" "$1" 
 }
 
-# Function to check and install dependencies
-install_dependencies() {
-    local dependencies=("$@")  # Accepts a list of dependencies as arguments
-
-    for pkg in "${dependencies[@]}"; do
-        # Check if it's an AUR package
-        if [[ "$pkg" =~ ^[a-zA-Z0-9_-]+-git$ ]] || [[ "$pkg" == "yay" ]]; then
-            # Check if yay or paru is installed
-            if command -v yay &> /dev/null; then
-                aur_helper="yay"
-            elif command -v paru &> /dev/null; then
-                aur_helper="paru"
-            else
-                print_error "Neither yay nor paru found. Installing yay..."
-                git clone https://aur.archlinux.org/yay-bin.git
-                cd yay-bin
-                makepkg -si --noconfirm
-                cd ..
-                rm -rf yay-bin
-                aur_helper="yay"
+# Function to install a package and handle conflicts
+install_package() {
+    local pkg="$1"
+    if ! sudo pacman -Sy --noconfirm "$pkg"; then
+        # Check for conflict in the error message
+        if pacman -Qi "$pkg" &> /dev/null; then
+            print_error "$pkg installation failed due to a conflict. Attempting to resolve..."
+            # Attempt to remove the conflicting package
+            echo "Removing conflicting package..."
+            sudo pacman -Rns "$pkg" || {
+                print_error "Failed to remove $pkg. Please check manually."
+                exit 1
+            }
+            # Retry installing the package
+            if ! sudo pacman -Sy --noconfirm "$pkg"; then
+                print_error "Failed to reinstall $pkg after resolving conflicts."
+                exit 1
             fi
-
-            # Install the AUR package if not already installed
-            if ! $aur_helper -Qi "$pkg" &> /dev/null; then
-                print_error "$pkg not found. Installing $pkg..."
-                if ! $aur_helper -S --noconfirm "$pkg"; then
-                    print_error "Failed to install $pkg. Please resolve conflicts and press any key to continue..."
-                    read -n1 -s -r -p "Press any key to continue after resolving conflicts..."
-                    # Pause before retrying
-                    sleep 5
-                fi
-            else
-                print_success "$pkg is already installed. Skipping installation."
-            fi
-
         else
-            # Otherwise, it's a pacman package
-            if ! pacman -Qi "$pkg" &> /dev/null; then
-                print_error "$pkg not found. Installing $pkg..."
-                if ! sudo pacman -Sy --noconfirm "$pkg"; then
-                    print_error "Failed to install $pkg. Please resolve conflicts and press any key to continue..."
-                    read -n1 -s -r -p "Press any key to continue after resolving conflicts..."
-                    # Pause before retrying
-                    sleep 5
-                fi
-            else
-                print_success "$pkg is already installed. Skipping installation."
-            fi
+            print_error "Failed to install $pkg due to an unknown error."
+            exit 1
         fi
-    done
+    else
+        print_success "$pkg installed successfully."
+    fi
 }
 
-# Check for and install base dependencies
-base_dependencies=("git" "wget" "unzip")
-install_dependencies "${base_dependencies[@]}"
-
-printf "$(tput setaf 2) Welcome to the Arch Linux Hyprland installer!\n $(tput sgr0)"
-
-### Install Required Packages ###
-read -n1 -rep "${CAT} Install required packages from README.md? (y/n)" inst_pkgs
-if [[ $inst_pkgs =~ ^[Yy]$ ]]; then
-    required_pkgs=("alacritty" "discord" "flameshot" "gparted" "google-chrome" "grimblast-git" \
-                   "nwg-look" "pamixer" "papirus-icon-theme" "pavucontrol" "rsync" "rofi" "sddm-git" \
-                   "thunar" "thunar-archive-plugin" "thunar-media-tags-plugin" "thunar-shares-plugin" \
-                   "thunar-vcs-plugin" "thunar-volman" "zoxide" "fzf" "swww" "p7zip")
-    install_dependencies "${required_pkgs[@]}"
-fi
-
-### Install Hyprland Dependencies ###
-read -n1 -rep "${CAT} Install Hyprland dependencies? (y/n)" inst_hypr
-if [[ $inst_hypr =~ ^[Yy]$ ]]; then
-    hyprland_dependencies=("aquamarine" "gdb" "hyprcursor" "hyprlang" "hyprutils-git" \
-                           "hyprwayland-scanner" "libdisplay-info" "libfixes" "libinput" \
-                           "libliftoff" "libxcb" "libxcomposite" "libxkbcommon" "libxrender" \
-                           "meson" "ninja" "pango" "pixman" "seatd" "tomlplusplus" \
-                           "wayland-protocols" "xcb-proto" "xcb-util" "xcb-util-errors" \
-                           "xcb-util-keysyms" "xcb-util-wm" "xorg-xinput" "xorg-xwayland")
-    install_dependencies "${hyprland_dependencies[@]}"
-fi
-
-### Install CMake and Build Hyprland ###
-read -n1 -rep "${CAT} Install CMake and build Hyprland? (y/n)" inst_cmake
-if [[ $inst_cmake =~ ^[Yy]$ ]]; then
-    build_tools=("cmake" "make")
-    install_dependencies "${build_tools[@]}"
-
-    # Build Hyprland
-    git clone --recursive https://github.com/hyprwm/Hyprland
-    cd Hyprland
-    make all && sudo make install
-    cd ..
-    rm -rf Hyprland
-fi
+# Check and install global dependencies
+global_dependencies=("wget" "unzip" "fc-list")
+for pkg in "${global_dependencies[@]}"; do
+    if ! pacman -Qi "$pkg" &> /dev/null; then
+        print_error "$pkg not found. Installing $pkg..."
+        install_package "$pkg"
+    else
+        print_success "$pkg is already installed. Skipping installation."
+    fi
+done
 
 ### Install Fonts ###
-read -n1 -rep "${CAT} Install Meslo Nerd Font? (y/n)" inst_fonts
+read -n1 -rep "${CAT} Install Meslo Nerd Font? (y/n) " inst_fonts
 if [[ $inst_fonts =~ ^[Yy]$ ]]; then
     FONT_DIR="$HOME/.local/share/fonts"
     FONT_ZIP="$FONT_DIR/Meslo.zip"
     FONT_URL="https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/Meslo.zip"
 
-    # Check if Meslo Nerd-font is already installed
     if fc-list | grep -qi "Meslo"; then
         print_success "Meslo Nerd-fonts are already installed."
     else
@@ -141,5 +93,95 @@ if [[ $inst_fonts =~ ^[Yy]$ ]]; then
     fi
 fi
 
-# Script completion
+### Install Required Packages ###
+read -n1 -rep "${CAT} Install required packages from README.md? (y/n) " inst_pkgs
+if [[ $inst_pkgs =~ ^[Yy]$ ]]; then
+    required_pkgs=("alacritty" "discord" "flameshot" "gparted" "google-chrome" "grimblast-git" \
+    "nwg-look" "pamixer" "papirus-icon-theme" "pavucontrol" "rsync" "rofi" "sddm-git" \
+    "thunar" "thunar-archive-plugin" "thunar-media-tags-plugin" "thunar-shares-plugin" \
+    "thunar-vcs-plugin" "thunar-volman" "zoxide" "fzf" "swww" "unzip" "p7zip")
+
+    for pkg in "${required_pkgs[@]}"; do
+        install_package "$pkg"
+    done
+else
+    print_success "No packages installed from README.md."
+fi
+
+### Install Hyprland Dependencies ###
+read -n1 -rep "${CAT} Install Hyprland dependencies? (y/n) " inst_hypr
+if [[ $inst_hypr =~ ^[Yy]$ ]]; then
+    hypr_dependencies=("aquamarine" "gdb" "hyprcursor" "hyprlang" "hyprutils-git" "hyprwayland-scanner" \
+    "libdisplay-info" "libfixes" "libinput" "libliftoff" "libxcb" "libxcomposite" \
+    "libxkbcommon" "libxrender" "meson" "ninja" "pango" "pixman" "seatd" \
+    "tomlplusplus" "wayland-protocols" "xcb-proto" "xcb-util" "xcb-util-errors" \
+    "xcb-util-keysyms" "xcb-util-wm" "xorg-xinput" "xorg-xwayland")
+
+    for pkg in "${hypr_dependencies[@]}"; do
+        install_package "$pkg"
+    done
+else
+    print_success "No Hyprland dependencies installed."
+fi
+
+### Install CMake and Build Hyprland ###
+read -n1 -rep "${CAT} Install CMake and build Hyprland? (y/n) " inst_hyprbuild
+if [[ $inst_hyprbuild =~ ^[Yy]$ ]]; then
+    git clone --recursive https://github.com/hyprwm/Hyprland
+    cd Hyprland
+    make all && sudo make install
+    print_success "Hyprland built and installed successfully."
+    cd ..
+else
+    print_success "Skipped building Hyprland."
+fi
+
+### Install Starship ###
+read -n1 -rep "${CAT} Install Starship? (y/n) " inst_starship
+if [[ $inst_starship =~ ^[Yy]$ ]]; then
+    curl -sS https://starship.rs/install.sh | sh
+    echo -e "\neval \"\$(starship init bash)\"" >> ~/.bashrc
+    echo -e "eval \"\$(zoxide init bash)\"" >> ~/.bashrc
+    print_success "Starship installed and configured."
+else
+    print_success "Starship installation skipped."
+fi
+
+### Enable SDDM Autologin ###
+read -n1 -rep "${CAT} Would you like to enable SDDM autologin? (y,n) " SDDM
+if [[ $SDDM =~ ^[Yy]$ ]]; then
+    LOC="/etc/sddm.conf"
+    echo -e "The following has been added to $LOC.\n"
+    echo -e "[Autologin]\nUser = $(whoami)\nSession=hyprland" | sudo tee -a $LOC
+    echo -e "\nEnabling SDDM service...\n"
+    sudo systemctl enable sddm
+    sleep 3
+fi
+
+# BLUETOOTH
+read -n1 -rep "${CAT} OPTIONAL - Would you like to install Bluetooth packages? (y/n) " BLUETOOTH
+if [[ $BLUETOOTH =~ ^[Yy]$ ]]; then
+    blue_pkgs=("bluez" "bluez-utils" "blueman")
+    for pkg in "${blue_pkgs[@]}"; do
+        install_package "$pkg"
+    done
+    printf "Activating Bluetooth Services...\n"
+    sudo systemctl enable --now bluetooth.service
+else
+    printf "${YELLOW} No Bluetooth packages installed..\n"
+fi
+
+# Script Completion
 printf "\n${GREEN} Installation Completed.\n"
+echo -e "${GREEN} You can start Hyprland by typing Hyprland (note the capital H).\n"
+read -n1 -rep "${CAT} Would you like to start Hyprland now? (y,n) " HYP
+if [[ $HYP =~ ^[Yy]$ ]]; then
+    if command -v Hyprland >/dev/null; then
+        exec Hyprland
+    else
+        print_error "Hyprland not found. Please make sure Hyprland is installed by checking install.log."
+        exit 1
+    fi
+else
+    exit
+fi
